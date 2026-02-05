913344df3c8bb977dcebca6abcc425423728537a
senn-ESAT <senn@esat-alumni.com>
2026-02-05 20:18:09 +0100

    Add files via upload

--- Desarrollo/postNavidad/FinTutorial/ElJuegoDeLaVida/JDLV.cc
+++ Desarrollo/postNavidad/FinTutorial/ElJuegoDeLaVida/JDLV.cc
@@ -1,16 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <esat/time.h>
 #include <string.h>
+#include <esat/time.h>
 #include <esat/draw.h>
 #include <esat/window.h>
 #include <esat/input.h>
 
-Balls *Block = nullptr;
+bool *Block = nullptr;
 int Screen = 800, fps = 10;
 double current_time, last_time;
 int nBichos = 70, espacionBichos = Screen/nBichos;
+
+// bool per tile
+// de -1 -1 a +1 +1 sumar bool=true si 2-3 range entonces vive si no muere
+// 
+
+void clickCheck(){}
+
+void generacionBichos(){
+  for(int i = 0; i < nBichos*2; i++){
+    bool bicho = false;
+    if(rand()%2 == 0){
+      bicho = true;
+    }
+    Block = (bool*)realloc(bool, (i+1)*sizeof(bool));
+    *(Block+i) = bicho;
+  }
+}
+
+void dibujarBicho(){
+  for(int i = 0; i < nBichos*2; i++){
+    if((Block+i)){
+      esat::DrawSetFillColor(255,0,0);
+      
+    }
+  }
+}
+
+void fpsControl(){
+  do {
+    current_time = esat::Time();
+  } while((current_time - last_time) <= 1000.0 / fps);
+}
 
 int esat::main(int argc, char **argv) {
   if(argc == 4){
@@ -22,13 +54,17 @@ int esat::main(int argc, char **argv) {
     esat::WindowInit(Screen, Screen);
     WindowSetMouseVisibility(true);
 
+    generacionBichos();
+    
+
     while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
       last_time = esat::Time();
       esat::DrawBegin();
       esat::DrawClear(0,0,0);
       
+      // clickCheck();
+      dibujarBicho();
 
-      
       esat::DrawEnd();  	
       esat::WindowFrame();
       
