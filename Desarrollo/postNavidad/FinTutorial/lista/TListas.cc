6f280f16b1de6e53568fc9194c14561983f38158
senn-ESAT <senn@esat-alumni.com>
2026-02-10 20:27:43 +0100

    LISTA

---
+++ Desarrollo/postNavidad/FinTutorial/lista/TListas.cc
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct TLista{
+  int info;
+  TLista *prox;
+};
+
+void CrearLista(TLista **lista){
+  lista = nullptr;
+}
+
+void InsertarLista(TLista **lista, int a){
+  TLista *aux;
+  aux = (TLista*)malloc(1*sizeof(TLista));
+  
+  aux->prox = *lista;
+  aux->info = a;
+  *lista = aux;
+}
+
+void MuestraLista(TLista *lista){
+  TLista *p;
+  for(p = lista; p != nullptr; p = p->prox){
+    printf(" %d, ", p->info);
+  }
+}
+// TO-DO no funcica
+TLista* IndexaLista(TLista *lista, int posicion){
+  TLista *p = nullptr;
+  for(int i = 0; i < posicion && lista != nullptr; lista = lista->prox){
+    if(i = posicion){
+      p = lista;
+    }
+    i++;
+  }
+  printf(" %d ", p->info);
+  return p;
+}
