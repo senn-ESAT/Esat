48580cb2a86e52f2fcce343426d04d590b6a8ab9
MATI2D <senn@esat-alumni.com>
2026-05-27 15:10:10 +0200

    practica sql

--- DB/PracticaSQL/Practica.sql
+++ DB/PracticaSQL/Practica.sql
@@ -7,7 +7,7 @@
 SELECT U.USR_NOMBRE, U.USR_PAIS_ORIGEN, U.USR_CAMPEONATOS_JUGADOS
 FROM USUARIOS AS U
 WHERE U.USR_PUNTOS_GANADOS > 1000 AND U.USR_FECHA_DESBLOQ < NOW()
-ORDER BY U.USR_PUNTOS_GANADOS DESC, U.USR_PAIS_ORIGEN;
+ORDER BY U.USR_PAIS_ORIGEN, U.USR_PUNTOS_GANADOS DESC;
 
 
 -- 2.Seleccionar las partidas que están en estado de pausadas durante más de un
@@ -15,7 +15,7 @@ ORDER BY U.USR_PUNTOS_GANADOS DESC, U.USR_PAIS_ORIGEN;
 -- comienzo de la partida y el nombre de la configuración de la misma.
 
 
-SELECT U.USR_NOMBRE, P.PART_FECHA_HORA_FIN, CP.CP_NOMBRE_CONFIG
+SELECT U.USR_NOMBRE, P.PART_FECHA_HORA_INICIO, CP.CP_NOMBRE_CONFIG
 FROM USUARIOS AS U, PARTIDAS AS P, config_partidas AS CP, partidas_usuarios AS PU
 WHERE PU.PU_USR_ID = U.USR_ID 
 AND PU.PU_PART_ID = P.PART_ID 
@@ -31,13 +31,16 @@ AND P.PART_FECHA_HORA_FIN IS NULL;
 -- campeonatos.
 
 
-SELECT MAX(CAMP_PUNTOS_GANADOS), 
-       MIN(CAMP_PUNTOS_GANADOS), 
-       AVG(CAMP_PUNTOS_GANADOS),
-       SUM(CAMP_TOTAL_INSCRIPTORES)
-FROM CAMPEONATOS WHERE CAMP_PUNTOS_GANADOS > 
-    (SELECT AVG(CAMP_PUNTOS_GANADOS) FROM CAMPEONATOS)
-GROUP BY CAMP_USR_CREADOR_CAMP;
+SELECT U.USR_NOMBRE,
+		 MAX(C.CAMP_PUNTOS_GANADOS), 
+       MIN(C.CAMP_PUNTOS_GANADOS), 
+       AVG(C.CAMP_PUNTOS_GANADOS),
+       SUM(C.CAMP_TOTAL_INSCRIPTORES)
+FROM campeonatos AS C, usuarios AS U
+WHERE U.USR_ID = C.CAMP_USR_CREADOR_CAMP AND
+	 C.CAMP_PUNTOS_GANADOS > 
+    (SELECT AVG(CM.CAMP_PUNTOS_GANADOS) FROM campeonatos AS CM)
+GROUP BY C.CAMP_USR_CREADOR_CAMP;
 
 
 -- 4.Indicar que jugadores se han apuntado a mas campeonatos, indicar el email del
@@ -49,7 +52,7 @@ SELECT U.USR_EMAIL, COUNT(*) AS apariciones
 FROM USUARIOS AS U, PARTICIPANTES_CAMPEONATOS AS P
 WHERE U.USR_ID = P.PTC_USR_ID
 GROUP BY U.USR_ID
-ORDER BY apariciones DESC LIMIT 1;
+ORDER BY apariciones DESC;
 
 
 -- esta, la 5 se tiene que hacer de 2 formas
@@ -60,9 +63,10 @@ FROM CAMPEONATOS
 WHERE CAMP_TOTAL_INSCRIPTORES = 0;
 
 SELECT C.CAMP_NOMBRE_CAMPEONATO
-FROM CAMPEONATOS AS C, PARTICIPANTES_CAMPEONATOS AS P
-WHERE C.CAMP_ID != P.PTC_CAMP_ID
-ORDER BY C.CAMP_ID DESC LIMIT 1;
+FROM CAMPEONATOS AS C
+LEFT JOIN PARTICIPANTES_CAMPEONATOS AS P
+    ON C.CAMP_ID = P.PTC_CAMP_ID
+WHERE P.PTC_CAMP_ID IS NULL;
 
 
 -- 6.Cuales son las configuraciones de partida menos seleccionadas y cuales las que
@@ -82,7 +86,7 @@ ORDER BY veces DESC, CP.CP_NOMBRE_CONFIG ASC;
 -- descendentemente y por nombre de usuario
 
 
-SELECT U.USR_NOMBRE
+SELECT U.USR_NOMBRE, U.USR_CAMPEONATOS_JUGADOS
 FROM USUARIOS AS U
 WHERE U.USR_PARTIDAS_JUGADAS = 0
 AND U.USR_CAMPEONATOS_JUGADOS > 5
@@ -96,20 +100,27 @@ ORDER BY U.USR_CAMPEONATOS_JUGADOS DESC;
 -- total de puntos obtenidos en los campeonatos, total de partidas jugadas online,
 -- número total de campeonatos ganados y el número total de campeonatos jugados.
 
+SELECT U.USR_EMAIL, SUM(C.CAMP_PUNTOS_GANADOS), U.USR_PARTIDAS_JUGADAS, count(*) AS VECES_GANADOR, U.USR_CAMPEONATOS_JUGADOS
+FROM USUARIOS AS U, CAMPEONATOS AS C, PARTICIPANTES_CAMPEONATOS AS PC
+WHERE U.USR_ID = PC.PTC_USR_ID
+AND PC.PTC_CAMP_ID = C.CAMP_ID
+AND PC.PTC_USER_GANADOR_SN = TRUE
+AND U.USR_PARTIDAS_JUGADAS > 10
+GROUP BY U.USR_ID
+
+
 -- 9.Para cada país indicar que jugadores han ganado más partidas y cuales menos
 -- partidas, mostrar los campos país origen, nombre usuario, partidas ganadas,
 -- partidas jugadas.
 
-
-SELECT U.USR_PAIS_ORIGEN, U.USR_NOMBRE, COUNT(*) as ganados
-FROM USUARIOS AS U, PARTIDAS_USUARIOS AS PU
-WHERE U.USR_ID = PU.PU_USR_ID
-AND PU.PU_POSICION_FINAL_EN_PARTIDA = 1;
-
-
-
--- 10. Insertar un registro en la tabla de campeonato.
-
--- 11. Modificar el registro creado en el punto 10.
-
--- 12. Eliminar el registro creado en el punto 11
+-- SELECT U.USR_PAIS_ORIGEN, U.USR_NOMBRE, SUM(PU.PU_PUNTOS_OBTENIDOS) AS PNT_USR
+-- FROM USUARIOS AS U, PARTIDAS_USUARIOS AS PU
+-- WHERE U.USR_ID = PU.PU_USR_ID
+-- JOIN 
+--   (SELECT UU.USER_ID,
+--       MAX(PUU.PU_PUNTOS_OBTENIDOS) AS TopPlayer
+--   FROM PURCHASES p
+--   GROUP BY p.customer)  ON y.customer = x.customer
+--                         AND y.max_total = x.total
+-- GROUP BY U.USR_ID
+-- ORDER BY U.USR_PAIS_ORIGEN,  PNT_USR DESC;
